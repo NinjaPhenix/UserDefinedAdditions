1c15ee71eb5756f4f93ebda4324fe278a0588f5e
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2019-07-15 03:24:11 +0100

    Merge validate and register method for convenience.

--- src/main/java/ninjaphenix/userdefinedadditions/CommonEntry.java
+++ src/main/java/ninjaphenix/userdefinedadditions/CommonEntry.java
@@ -1,12 +1,15 @@
 package ninjaphenix.userdefinedadditions;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import ninjaphenix.userdefinedadditions.config.Config;
 import ninjaphenix.userdefinedadditions.config.data.Item;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class CommonEntry implements ModInitializer
 {
@@ -19,15 +22,14 @@ public class CommonEntry implements ModInitializer
         LOGGER.info("[{}] Mod Initialized.", MOD_ID);
         Config.initialize();
         LOGGER.info("[{}] Config Initialized.", MOD_ID);
-        validateItems();
+        validate_and_registerItems();
     }
 
-    private void validateItems()
+    private void validate_and_registerItems()
     {
         // Lets create a local copy because we might be fixing a lot of identifiers.
         Config instance = Config.INSTANCE;
         Item[] items = instance.getItems();
-        ArrayList<Item> validItems = new ArrayList<>();
 
         for (Item item : items)
         {
@@ -36,18 +38,6 @@ public class CommonEntry implements ModInitializer
             // fix mod identifiers
 
             // if invalid log error and don't add item to list
-        }
-        this.registerItems(validItems.toArray(new Item[]{}));
-    }
-
-    private void registerItems(Item[] items)
-    {
-        for (Item item : items)
-        {
-            // Create Item.Settings
-                // Create Food Component
-                    // Create Status Effect Instances
-            // Register Item
         }
     }
 }
