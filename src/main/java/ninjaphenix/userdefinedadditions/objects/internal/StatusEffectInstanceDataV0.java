058b2dfb7fa1f2210f9da761124565113bd86508
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2019-09-24 14:10:34 +0100

    Start to implement StatusEffectInstanceDataV0

--- src/main/java/ninjaphenix/userdefinedadditions/objects/internal/StatusEffectInstanceDataV0.java
+++ src/main/java/ninjaphenix/userdefinedadditions/objects/internal/StatusEffectInstanceDataV0.java
@@ -1,5 +1,39 @@
 package ninjaphenix.userdefinedadditions.objects.internal;
 
-public class StatusEffectInstanceDataV0
+import blue.endless.jankson.JsonObject;
+import blue.endless.jankson.impl.Marshaller;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import ninjaphenix.userdefinedadditions.Serializer;
+
+public class StatusEffectInstanceDataV0 implements Serializer<StatusEffectInstanceDataV0, StatusEffectInstance>
 {
+    private Float chance;
+
+    @Override
+    public StatusEffectInstanceDataV0 read(JsonObject object)
+    {
+        return null;
+    }
+
+    @Override
+    public JsonObject write(StatusEffectInstanceDataV0 object, Marshaller marshaller)
+    {
+        return null;
+    }
+
+    /**
+     * Note, whilst this is mostly a complete object the float chance to get an effect isn't represented here.
+     *
+     * @see StatusEffectInstanceDataV0#getChance()
+     */
+    @Override
+    public StatusEffectInstance asRealObject()
+    {
+        return null;
+    }
+
+    /**
+     * @return The chance for this status effect to apply.
+     */
+    public Float getChance() { return chance; }
 }
